e7ea2a6976d25ab0d500b85aef8475f6209bbea9
vyteking <65077361+vyteking@users.noreply.github.com>
2020-08-19 17:24:25 +0900

    Update dice_roller.py

--- dice_roller.py
+++ dice_roller.py
@@ -1,7 +1,8 @@
 import random
 
 def main():
-  dice_rolls = 2
+  dice_rolls = int(input('How many dice would you like to roll? '))
+  dice_size = int(input('How many size are the dice? '))
   dice_sum = 0
   for i in range(0, dice_rolls):
     roll = random.randint(1, 6)
